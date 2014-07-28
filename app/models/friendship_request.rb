339ac42e59f39e37ac489445d019552b1cd29d95
Francesco Boffa <fra.boffa@gmail.com>
2014-07-29 00:02:31 +0200

    Enforced friendship request in one way.

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -5,10 +5,19 @@ class FriendshipRequest < ActiveRecord::Base
   validates_presence_of :user, :target
   validates_uniqueness_of :target, scope: :user
 
+  after_save :ensure_one_way
+
   def accept!
     transaction do
       delete
       Friendship.add_link user, target
+    end
+  end
+
+  def ensure_one_way
+    if FriendshipRequest.where(user: target, target: user).count != 0
+      errors[:target] << "has already been taken"
+      raise ActiveRecord::RecordInvalid.new self
     end
   end
 end
